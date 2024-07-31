33ca315691ba6b5c665c806e21a9d5cda6d5cbc9
Jayantm96 <134455119+Jayantm96@users.noreply.github.com>
2024-07-31 18:41:11 -0400

    All files done

---
+++ src/pages/MovieList.js
@@ -0,0 +1,34 @@
+
+import { Card } from '../components'
+import { useFetch } from '../hooks/useFetch';
+import {useTitle} from '../hooks/useTitle';
+
+
+export const MovieList = ({ apiPath, title }) => {
+
+
+
+
+    const { data: movies } = useFetch(apiPath);
+    useTitle(title)
+
+
+    return (
+        <main>
+            <section className="m-2 max-w-7xl mx-auto py-7">
+
+                <div className="flex justify-start flex-wrap other:justify-evenly">
+
+                    {movies.map((movie) => (
+                        <Card key={movie.id} movie={movie} />
+                    ))}
+
+
+
+                </div>
+
+            </section>
+        </main>
+    )
+}
+
